9be1db169d25815ffe31a3cbe91853f4de6793fd
Timur Kharin <timurx404@gmail.com>
2023-05-26 11:12:40 +0300

    update

--- lib/login.dart
+++ lib/login.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-import 'auth/auth_manager.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
