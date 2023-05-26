1ae1d886f85f18ca1680c4b705316cd944ccfcda
Daniil Zimin <6890213-daniilzimin4@users.noreply.gitlab.com>
2023-05-26 18:04:43 +0300

    simple user info widget version 3.0

--- lib/ui/widgets/user_info.dart
+++ lib/ui/widgets/user_info.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 
-class UserProfilePage extends StatelessWidget {
-  final String username;
-  final String email;
-  final String bio;
-  final String profileImage;
+class UserProfilePage extends StatefulWidget {
+  @override
+  _UserProfilePageState createState() => _UserProfilePageState();
+}
 
-  UserProfilePage({
-    required this.username,
-    required this.email,
-    required this.bio,
-    required this.profileImage,
-  });
+class _UserProfilePageState extends State<UserProfilePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _slideAnimation;
+
+  bool _isProfileOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _slideAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleProfile() {
+    setState(() {
+      _isProfileOpen = !_isProfileOpen;
+
+      if (_isProfileOpen) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +54,80 @@ class UserProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('User Profile'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: NetworkImage(profileImage),
-                radius: 50.0,
+      body: Stack(
+        children: [
+          // Content Widget
+          const Positioned.fill(
+            child: Center(
+              child: Text(
+                'Main Content',
+                style: TextStyle(fontSize: 20.0),
               ),
-              SizedBox(height: 16.0),
-              Text(
-                username,
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          // Profile Widget
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+            top: _isProfileOpen ? 0 : MediaQuery.of(context).size.height * 0.6,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: Offset(0, 1),
+                end: Offset(0, 0),
+              ).animate(CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              )),
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(16.0),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://example.com/profile_image.jpg'),
+                      radius: 50.0,
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      'John Doe',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'johndoe@example.com',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      'Hello, I\'m John Doe. Nice to meet you!',
+                      style: TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 8.0),
-              Text(
-                email,
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                bio,
-                style: TextStyle(fontSize: 16.0),
-                textAlign: TextAlign.center,
-              ),
-            ],
+            ),
           ),
-        ),
+
+          // Button Widget
+          Positioned(
+            bottom: 16.0,
+            right: 16.0,
+            child: FloatingActionButton(
+              onPressed: _toggleProfile,
+              child: Icon(_isProfileOpen ? Icons.close : Icons.person),
+            ),
+          ),
+        ],
       ),
     );
   }
